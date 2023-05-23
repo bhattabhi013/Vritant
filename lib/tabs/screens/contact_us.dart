4c556e6c4e8ef042b768e064452b3ef818757a97
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-05-23 19:21:56 +0530

    created a basic contact us and about us page

---
+++ lib/tabs/screens/contact_us.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ContactUs extends StatelessWidget {
+  const ContactUs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Contact Us Page'),
+      ),
+    );
+  }
+}
