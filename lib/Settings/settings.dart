b0643cd2355bae532a28287bc91e8d5d0e0607f7
Jaivardhan Shukla <93859359+jaivsh@users.noreply.github.com>
2023-08-01 10:03:12 +0530

    Update settings.dart

--- lib/Settings/settings.dart
+++ lib/Settings/settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:vritant/Settings/licenses.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -11,6 +13,8 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+
+  final String issuesurl = 'https://github.com/bhattabhi013/Vritant/issues';
   final String subject = 'Hey, check out Vritant, an amazing application that is built upon a comprehensive analysis of children\'s educational needs and the challenges they face while studying. It helps children learn rhymes faster in an easy and lucrative way!- https://github.com/bhattabhi013/Vritant';
   @override
   Widget build(BuildContext context) {
@@ -28,16 +32,28 @@ class _SettingsPageState extends State<SettingsPage> {
                   title: const Text('Share Vritant'),
                   value: const Text('Help to spread a word about the application!'),
                   onPressed: (t) {
-                    Share.share(subject, subject: 'Check out this amazing application!');
-    }
-    ,
+                    Share.share(subject, subject: 'Check out this amazing application!');},
                 ),
-
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.support),
+                  title: const Text('Support Development'),
+                  value: const Text('See latest issues and contribute to the project!'),
+                  onPressed: (t) {
+                    _launchURL();
+                  },
+                ),
               ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  _launchURL() async {
+    final Uri url = Uri.parse(issuesurl);
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $issuesurl');
+    }
   }
 }
