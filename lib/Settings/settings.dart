65e242f1ed72b51dc733e3d71741e8c3fd61d6a3
Jaivardhan Shukla <93859359+jaivsh@users.noreply.github.com>
2023-08-01 16:22:04 +0530

    Update settings.dart

--- lib/Settings/settings.dart
+++ lib/Settings/settings.dart
@@ -29,8 +29,15 @@ class _SettingsPageState extends State<SettingsPage> {
                   value: const Text('Help to spread a word about the application!'),
                   onPressed: (t) {
                     Share.share(subject, subject: 'Check out this amazing application!');
-    }
-    ,
+    },
+                  SettingsTile.navigation(
+                  leading: const Icon(Icons.contact_emergency_outlined),
+                  title: const Text('Contact Us'),
+                  value: const Text('Having Issues using Vritant? Contact us now!'),
+                  onPressed: (t) {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => ContactScreen()));
+                  },
+                ),
                 ),
 
               ],
