c08fcc2ebff3268fe22398d2f3fb437bc3ef64b0
Jaivardhan Shukla <93859359+jaivsh@users.noreply.github.com>
2023-08-01 09:17:40 +0530

    Update settings.dart

--- lib/Settings/settings.dart
+++ lib/Settings/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:vritant/Settings/licenses.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+
   final String subject = 'Hey, check out Vritant, an amazing application that is built upon a comprehensive analysis of children\'s educational needs and the challenges they face while studying. It helps children learn rhymes faster in an easy and lucrative way!- https://github.com/bhattabhi013/Vritant';
   @override
   Widget build(BuildContext context) {
@@ -28,11 +30,16 @@ class _SettingsPageState extends State<SettingsPage> {
                   title: const Text('Share Vritant'),
                   value: const Text('Help to spread a word about the application!'),
                   onPressed: (t) {
-                    Share.share(subject, subject: 'Check out this amazing application!');
-    }
-    ,
+                    Share.share(subject, subject: 'Check out this amazing application!');},
                 ),
-
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.document_scanner_outlined),
+                  title: const Text('Licenses'),
+                  value: const Text('See application\'s licenses'),
+                  onPressed: (t) {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => LicensesScreen()));
+                  },
+                ),
               ],
             ),
           ],
